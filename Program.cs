0d02d6bb37531886e55259fe5b2628b9456eae7c
Admin <Admin@DESKTOP-A8NE9AM>
2024-04-04 15:49:54 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//Programmer: Brian Lee
+//Date: 04/04/2024
+
+//Title: Progamming 120 Lecture1 Notes
+
+namespace Lecture1NotesBLee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Brian Lee");
+            Console.WriteLine("Programming 120");
+            Console.WriteLine("I play a lot of games");
+            Console.WriteLine();
+            /* What kind of errod does a red squiggly line in your code represent: It is the Compile Time Error
+             * What is the difference betweeen Console.Write() and Console.WriteLine(): Double Quotations
+             * What services do we use to save our code online: Github
+             * When you see { and }, this indicates a bunch of code is grouped together: Code Block
+             */
+
+
+
+        }//End of Main
+    }// End of Program
+}//End of namespace
+
