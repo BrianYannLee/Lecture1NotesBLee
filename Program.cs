35ebc5e4bdc312e2acc047153a414a2b01cdb72e
Admin <Admin@DESKTOP-A8NE9AM>
2024-04-09 19:14:55 -0700

    Made Assignment 1 a method and calling it in main

--- Program.cs
+++ Program.cs
@@ -3,25 +3,32 @@
 
 //Title: Progamming 120 Lecture1 Notes
 
-namespace Lecture1NotesBLee
+namespace CSI120AssignmentsBLee
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Assignment1();
+
+
+        }//End of Main
+        static void Assignment1()
+        {
             Console.WriteLine("Brian Lee");
             Console.WriteLine("Programming 120");
             Console.WriteLine("I play a lot of games");
             Console.WriteLine();
-            /* What kind of errod does a red squiggly line in your code represent: It is the Compile Time Error
+            /* What kind of error does a red squiggly line in your code represent: It is the Compile Time Error
              * What is the difference betweeen Console.Write() and Console.WriteLine(): Double Quotations
              * What services do we use to save our code online: Github
              * When you see { and }, this indicates a bunch of code is grouped together: Code Block
              */
 
 
+        }//end of Assignment1
 
-        }//End of Main
-    }// End of Program
+    }// End of class
+
 }//End of namespace
 
